c9fc17876b8d37a3b61c5d517056abfbf2d8d8af
loic <loic.gstrn@gmail.com>
2023-09-28 17:18:02 +0200

    Tp Recipes

--- Typescript/filter_recipes/assets/js/main.js
+++ Typescript/filter_recipes/assets/js/main.js
@@ -1,12 +1,80 @@
 import { recipes } from "./data/recipes.js";
-const recipesList = document.querySelector("#recipesList");
-const mesRecettes = recipes;
-console.table(mesRecettes);
-const displayRecipe = () => {
-    for (const key in object) {
-        if (Object.prototype.hasOwnProperty.call(object, key)) {
-            const element = object[key];
-        }
+const recipesContainer = document.querySelector("#recipesList");
+const modalName = document.querySelector("#recipeName");
+const recipeDetail = document.querySelector(".myModal");
+const prepTime = document.querySelector("#prepTime");
+const cookTime = document.querySelector("#cookTime");
+const serving = document.querySelector("#servings");
+const ingredients = document.querySelector("#ingredients");
+const instructions = document.querySelector("#instructions");
+const closeModal = document.querySelector("#editContactClose");
+const preparationTime = document.querySelector("#preparationTime");
+const cookingTime = document.querySelector("#cookingTime");
+const recipeName = document.querySelector("#recipeName");
+const myRecipes = [];
+for (const key in recipes) {
+    myRecipes.push(recipes[key]);
+}
+// console.table(myRecipes[1].instructions);
+// console.log(myRecipes[1].name);
+const displayRecipes = () => {
+    for (let key in myRecipes) {
+        const newButton = document.createElement("button");
+        newButton.innerHTML =
+            `
+        <span>${myRecipes[key].name}</span>
+        <hr>
+        <div class="d-flex justify-content-around">
+            <div class="d-flex">
+                <img src="./assets/img/chef-hat.svg" alt="chefHat">
+                <span class="ms-2">${myRecipes[key].prepTime}</span>
+            </div>
+            <div class="d-flex">
+                <img src="./assets/img/fire.svg" alt="fire">
+                <span class="ms-2">${myRecipes[key].cookTime}</span>
+            </div>
+        </div>
+        `;
+        newButton.className = "btn btn-outline-light w-100 my-2";
+        newButton.addEventListener('click', () => {
+            recipeDetail.style.display = "block";
+            // console.log(myRecipes.indexOf(myRecipes[key]));
+            modalRecipeDetail(myRecipes.indexOf(myRecipes[key]));
+        });
+        recipesContainer.appendChild(newButton);
     }
 };
-displayRecipe();
+const modalRecipeDetail = (index) => {
+    modalName.textContent = myRecipes[index].name;
+    prepTime.textContent = myRecipes[index].prepTime;
+    cookTime.textContent = myRecipes[index].cookTime;
+    serving.textContent = `${myRecipes[index].servings.toString()} serving(s)`;
+    listIngredients(index);
+    listInstructions(index);
+};
+const listIngredients = (index) => {
+    myRecipes[index].ingredients.forEach(ingredient => {
+        const liIngredient = document.createElement("li");
+        liIngredient.textContent = `${ingredient.name} (${ingredient.amount})`;
+        ingredients.appendChild(liIngredient);
+    });
+};
+const listInstructions = (index) => {
+    myRecipes[index].instructions.forEach(instruction => {
+        const liInstruction = document.createElement("li");
+        liInstruction.textContent = `${instruction}`;
+        instructions.appendChild(liInstruction);
+    });
+};
+closeModal.addEventListener('click', () => {
+    recipeDetail.style.display = "none";
+    ingredients.textContent = '';
+    instructions.textContent = '';
+});
+/* preparationTime.addEventListener('click', () => {
+    console.log(preparationTime.value);
+}) */
+/* cookingTime.addEventListener('click', () => {
+    console.log(cookingTime.value);
+})  */
+displayRecipes();
--- Typescript/filter_recipes/assets/ts/main.ts
+++ Typescript/filter_recipes/assets/ts/main.ts
@@ -1,19 +1,96 @@
 import { recipes } from "./data/recipes.js";
 import { Recipe } from "./interface/Recipe.js";
 
-const recipesList = document.querySelector("#recipesList") as HTMLDivElement
+const recipesContainer = document.querySelector("#recipesList") as HTMLDivElement
+const modalName = document.querySelector("#recipeName") as HTMLHeadingElement
+const recipeDetail = document.querySelector(".myModal") as HTMLDivElement
+const prepTime = document.querySelector("#prepTime") as HTMLSpanElement
+const cookTime = document.querySelector("#cookTime") as HTMLSpanElement
+const serving = document.querySelector("#servings") as HTMLSpanElement
+const ingredients = document.querySelector("#ingredients") as HTMLUListElement
+const instructions = document.querySelector("#instructions") as HTMLUListElement
+const closeModal = document.querySelector("#editContactClose") as HTMLElement
+const preparationTime = document.querySelector("#preparationTime") as HTMLInputElement
+const cookingTime = document.querySelector("#cookingTime") as HTMLInputElement
+const recipeName = document.querySelector("#recipeName") as HTMLInputElement
 
-const mesRecettes: Recipe[] = recipes
+const myRecipes: Recipe[] = []
 
-console.table(mesRecettes);
+for (const key in recipes) {
+    myRecipes.push(recipes[key])
+}
+// console.table(myRecipes[1].instructions);
+// console.log(myRecipes[1].name);
 
-const displayRecipe = () => {
-    for (const key in object) {
-        if (Object.prototype.hasOwnProperty.call(object, key)) {
-            const element = object[key];
-            
-        }
-    }
+const displayRecipes = () => {
+    for (let key  in myRecipes){
+        const newButton = document.createElement("button")
+        newButton.innerHTML =
+        `
+        <span>${myRecipes[key].name}</span>
+        <hr>
+        <div class="d-flex justify-content-around">
+            <div class="d-flex">
+                <img src="./assets/img/chef-hat.svg" alt="chefHat">
+                <span class="ms-2">${myRecipes[key].prepTime}</span>
+            </div>
+            <div class="d-flex">
+                <img src="./assets/img/fire.svg" alt="fire">
+                <span class="ms-2">${myRecipes[key].cookTime}</span>
+            </div>
+        </div>
+        `
+        newButton.className = "btn btn-outline-light w-100 my-2"
+        newButton.addEventListener('click', () => {
+            recipeDetail.style.display = "block"
+            // console.log(myRecipes.indexOf(myRecipes[key]));
+            modalRecipeDetail(myRecipes.indexOf(myRecipes[key]))
+        })
+        recipesContainer.appendChild(newButton)
+    } 
 }
 
-displayRecipe()
+const modalRecipeDetail = (index: number) => {
+    modalName.textContent = myRecipes[index].name
+    prepTime.textContent = myRecipes[index].prepTime
+    cookTime.textContent = myRecipes[index].cookTime
+    serving.textContent = `${myRecipes[index].servings.toString()} serving(s)`
+    listIngredients(index)
+    listInstructions(index)
+}
+
+const listIngredients = (index: number) => {
+    myRecipes[index].ingredients.forEach(
+        ingredient => {
+            const liIngredient = document.createElement("li")
+            liIngredient.textContent = `${ingredient.name} (${ingredient.amount})`
+            ingredients.appendChild(liIngredient)
+        } 
+    )
+}
+
+const listInstructions = (index: number) => {
+    myRecipes[index].instructions.forEach(
+        instruction => {
+            const liInstruction = document.createElement("li")
+            liInstruction.textContent = `${instruction}`
+            instructions.appendChild(liInstruction)
+        }
+    )
+}
+
+closeModal.addEventListener('click', () => {
+    recipeDetail.style.display = "none";
+    ingredients.textContent = ''
+    instructions.textContent = ''
+});
+
+/* preparationTime.addEventListener('click', () => {
+    console.log(preparationTime.value);
+}) */
+
+/* cookingTime.addEventListener('click', () => {
+    console.log(cookingTime.value);
+})  */
+
+displayRecipes()
