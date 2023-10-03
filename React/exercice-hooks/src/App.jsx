27ac44a8b1f28bf393e23dd11bfcb1acc41b0f4b
loic <loic.gstrn@gmail.com>
2023-10-03 11:05:04 +0200

    add welcomeComponenentClass in hooks exercice

--- React/exercice-hooks/src/App.jsx
+++ React/exercice-hooks/src/App.jsx
@@ -1,10 +1,12 @@
 import WelcomeComponent from "./components/WelcomeComponent";
+import WelcomeComponentClass from "./components/WelcomeComponentClass";
 import "bootstrap/dist/css/bootstrap.min.css"
 
 function App() {
   return (
     <div className="container my-2">
       <WelcomeComponent/>
+      <WelcomeComponentClass/>
     </div>
   );
 }
---
+++ React/exercice-hooks/src/components/WelcomeComponentClass.jsx
@@ -0,0 +1,39 @@
+import { PureComponent } from "react";
+
+class WelcomeComponentClass extends PureComponent {
+    constructor(props) {
+        super(props)
+        this.state = {
+            firstname: "loïc",
+            lastname: "gastrin"
+        }
+    }
+
+    changeFirstname (event) {
+        this.setState(prevState => ({...prevState, firstname: event.target.value}))
+    }
+    changeLastname (event) {
+        this.setState(prevState => ({...prevState, lastname: event.target.value}))
+    }
+
+
+    render() {
+        return(
+            <>
+                <div>
+                <label for="firstname" class="form-label">Prénom :</label>
+                <input type="text" className="form-control" for="firstname" onInput={this.changeFirstname.bind(this)}/>
+                </div>
+                <div>
+                    <label for="lastname" class="form-label">Nom :</label>
+                    <input type="text" className="form-control" for="lastname" onInput={this.changeLastname.bind(this)}/>
+                </div>
+                <p className="text-center my-2">
+                    Bonjour <b><span className="text-capitalize">{this.state.firstname} {this.state.lastname.toUpperCase()}</span></b>, bienvenue sur l'application !
+                </p>
+            </>
+        )   
+    }
+}
+
+export default WelcomeComponentClass
