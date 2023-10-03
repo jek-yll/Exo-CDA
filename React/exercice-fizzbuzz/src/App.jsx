d88a61274ce4234024d1055169300aa982744850
loic <loic.gstrn@gmail.com>
2023-10-03 14:14:22 +0200

    fizzbuzz modif

--- React/exercice-fizzbuzz/src/App.jsx
+++ React/exercice-fizzbuzz/src/App.jsx
@@ -1,12 +1,24 @@
+import { useState } from 'react'
 import './App.css'
 import FizzBuzz from './components/FizzBuzz'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
+  const [max, setMax] = useState(42)
+
+  
+
+  const changeMax = (event) => {
+    setMax(event.target.value)
+  }
 
   return (
     <>
-      <FizzBuzz max={42}/>
+      <div className='d-flex align-items-center'>
+        <label className="col-auto mx-4" htmlFor="max">Nombre max </label>
+        <input className="form-control" type="number" name="max" id="max" onInput={changeMax}/>
+      </div>
+      <FizzBuzz max={max}/>
     </>
   )
 }
